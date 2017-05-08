a0125743a2e413664336508abbd04d598b0d16f9
nicrausaz <n.crausaz99@gmail.com>
2017-05-08 13:04:59 +0200

    started getting infos from web page

--- bot.py
+++ bot.py
@@ -1,63 +1,41 @@
 from telegram.ext import Updater, CommandHandler
 import requests
+from bs4 import BeautifulSoup
+
+rankingPage = requests.get('https://www.basketplan.ch/showRankingForLeague.do?leagueHoldingId=5621')
+
+rankingPageTextContent = rankingPage.text
+soup = BeautifulSoup(rankingPageTextContent, 'html.parser')
+rankings = soup.findAll("td", { "class" : "txt8" })
+
 
 def start(bot, update):
-    update.message.reply_text('Welcome ! Send /ranking to get the ranking of Morges Basket!')
+    update.message.reply_text('Welcome ! Send me /ranking to get the ranking of Morges Basket!')
 
 def ranking(bot, update):
-    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name)))
+    categories = {'Rank', 'Team', 'Games', 'Win', 'Loose', 'FF', 'Scored', 'Encaissé','Score', 'Points'}
+    update.message.reply_text('Rankings:')
+    for rank in rankings :
+        update.message.reply_text(rank.text)
 
-# get ranking from https://www.basketplan.ch/showRankingForLeague.do?leagueHoldingId=5621
+    
+
+
+# def ranksInfos():
+
+ 
 
 updater = Updater('388939599:AAEEarbs86EH7_jCmhQY2WAzzJa-6Wg0hrk')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('ranking', hello))
+updater.dispatcher.add_handler(CommandHandler('ranking', ranking))
 
 updater.start_polling()
 
-# exemple of recup
+# exemple of recup                      
 '''
 <table border="0" cellspacing="0" cellpadding="2" width="740" class="forms">
 
-		<tr>
-        	
-        		<td colspan="12" align="right">
-					<a href="showLeagueSchedule.do?leagueHoldingId=5621&amp;federationId=6" class="a_txt8">
-	                    Calendrier de championnat
-	                </a>&nbsp;
-	                <a href="showRankingForLeague.do?leagueHoldingId=5621&amp;federationId=6&amp;hideMenu=true&amp;perspective=default" 
-	                class="a_txt8">
-	                    <img src="img/Link.gif" border="0" alt="Voir cette page comme elle pourrait être intégrée comme IFrame">
-	                </a>
-	        	</td>
-        	
-        	
-        	<td align="center">
-	        	<a href="showRankingForLeague.do?leagueHoldingId=5621&amp;federationId=6&amp;isMobile=true"  class="a_txt8">
-	                   <img src="img/mobile.gif" border="0">
-	            </a>
-            </td>
-        </tr>
-                
-        <tr>
-            <td class="txt8b" width="30"><SPAN class="margin">Classement</SPAN></td>
-            <td class="txt8b" width="400">Équipe</td>
-            <td class="txt8b" width="50">Matchs</td>
-            <td class="txt8b" width="50">V</td>
-            <td class="txt8b" width="50">D</td>
-            <td class="txt8b" width="50">FF</td>
-
-            <td class="txt8b" width="50">Marq</td>
-            <td class="txt8b" width="50">Enc</td>
-            <td class="txt8b" width="50">Sc.</td>
-            
-            
-            <td class="txt8b" width="50">Points</td>
-        </tr>
-    
-        
-        
             <tr onmouseover="highlightThisRowColor(this,'C0CDD6')" onmouseout="resetThisRowColor(this)">
                 <td class="txt8"><SPAN class="margin">1</SPAN></td>
                 <td class="txt8">
