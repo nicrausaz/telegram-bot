1fd1959755021fed6686970d0b3103c3817ce499
nicrausaz <n.crausaz99@gmail.com>
2017-05-09 20:18:01 +0200

    markdown msg

--- bot.py
+++ bot.py
@@ -1,23 +1,26 @@
+import telegram
 from telegram.ext import Updater, CommandHandler
 from bs4 import BeautifulSoup
 import requests
 
+# Request and soup
 rankingPage = requests.get('https://www.basketplan.ch/showRankingForLeague.do?leagueHoldingId=5621')
-
 rankingPageTextContent = rankingPage.text
 soup = BeautifulSoup(rankingPageTextContent, 'html.parser')
-
 rankings = soup.findAll("tr", {"onmouseout":"resetThisRowColor(this)"})
 
 
 def start(bot, update):
-    update.message.reply_text('Welcome ! \nSend me /ranking to get the ranking of Morges Basket\'s group!')
-
-def ranking(bot, update):
-    categories = ['Rank', 'Team', 'Games', 'Win', 'Loose', 'FF', 'Scored', 'Encaissé','Score', 'Points']
-    update.message.reply_text('Rankings:')
+    chat_id = bot.get_updates()[-1].message.chat_id
+    bot.send_message(chat_id=chat_id, text='Welcome ! \nSend me /ranking to get the ranking of Morges Basket\'s group!')
 
     
+    
+def ranking(bot, update):
+    chat_id = bot.get_updates()[-1].message.chat_id
+    
+    bot.send_message(chat_id=chat_id, text='Classement:')
+    categories = ['Place', 'Équipe', 'Matchs', 'Victoires', 'Défaites', 'FF', 'Marqué', 'Encaissé', 'Score', 'Points']
 
     for rank in rankings:
         rank = rank.text.split('\n')
@@ -25,10 +28,10 @@ def ranking(bot, update):
         text = ""
 
         for i in range(len(categories)) :          
+            text += "*%s* : _%s_ \r\n" % (categories[i], rank[i])
+        bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
 
-            text += "%s : %s \r\n" % (categories[i], rank[i])
-
-        update.message.reply_text(text)
+    
 def table(bot, update):
     button_list = [
     InlineKeyboardButton("col 1", ...),
@@ -45,3 +48,4 @@ updater.dispatcher.add_handler(CommandHandler('ranking', ranking))
 updater.dispatcher.add_handler(CommandHandler('table', table))
 
 updater.start_polling()
+    
