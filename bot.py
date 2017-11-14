30c1c747ccdd53dce001633c3ac5d514b7848f37
nicrausaz <n.crausaz99@gmail.com>
2017-11-14 16:38:26 +0100

    update url

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # Request and soup
-rankingPage = requests.get('https://www.basketplan.ch/showRankingForLeague.do?leagueHoldingId=5906')
+rankingPage = requests.get('https://www.basketplan.ch/showRankingForLeague.do?leagueHoldingId=5969')
 rankingPageTextContent = rankingPage.text
 soup = BeautifulSoup(rankingPageTextContent, 'html.parser')
 rankings = soup.findAll("tr", {"onmouseout":"resetThisRowColor(this)"})
@@ -14,11 +14,9 @@ def start(bot, update):
     chat_id = bot.get_updates()[-1].message.chat_id
     bot.send_message(chat_id=chat_id, text='Welcome ! \nSend me /ranking to get the ranking of Morges Basket\'s group!')
 
-    
-    
 def ranking(bot, update):
     chat_id = bot.get_updates()[-1].message.chat_id
-    
+
     bot.send_message(chat_id=chat_id, text='Classement:')
     categories = ['Place', 'Équipe', 'Matchs', 'Victoires', 'Défaites', 'FF', 'Marqué', 'Encaissé', 'Score', 'Points']
 
@@ -27,11 +25,10 @@ def ranking(bot, update):
         rank = [x for x in rank if x]
         text = ""
 
-        for i in range(len(categories)) :          
+        for i in range(len(categories)) :
             text += "*%s* : _%s_ \r\n" % (categories[i], rank[i])
         bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.MARKDOWN)
 
-    
 def table(bot, update):
     button_list = [
     InlineKeyboardButton("col 1", ...),
@@ -48,4 +45,3 @@ updater.dispatcher.add_handler(CommandHandler('ranking', ranking))
 updater.dispatcher.add_handler(CommandHandler('table', table))
 
 updater.start_polling()
-    
